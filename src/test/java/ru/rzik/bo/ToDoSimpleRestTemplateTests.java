63dfef86255e417560803fe823fa3f3e8e1f1a15
grbm <olejnec@gmail.com>
2025-02-02 19:25:11 +0300

    add annotation req args constr

--- src/test/java/ru/rzik/bo/ToDoSimpleRestTemplateTests.java
+++ src/test/java/ru/rzik/bo/ToDoSimpleRestTemplateTests.java
@@ -1,5 +1,7 @@
 package ru.rzik.bo;
 
+import lombok.extern.log4j.Log4j;
+import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -10,6 +12,7 @@ import ru.rzik.bo.model.Bo;
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@Slf4j
 public class ToDoSimpleRestTemplateTests {
 
     @Autowired
@@ -21,7 +24,7 @@ public class ToDoSimpleRestTemplateTests {
         bo.setName("12313");
         bo.setDescription("asdasdasd");
         ResponseEntity<Bo> responseEntity = restTemplate.postForEntity("/add", bo, Bo.class);
-        System.out.println(responseEntity.getBody());
+        log.info("responseEntity: {}", responseEntity);
         assertThat(responseEntity.getStatusCode().is2xxSuccessful()).isTrue();
     }
 }
