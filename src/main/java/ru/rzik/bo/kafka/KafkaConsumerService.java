dcf70c53b2c4c30ff5233c8b93a46cd7e57e96f4
grbm <olejnec@gmail.com>
2025-02-05 12:27:11 +0300

    edit db

--- src/main/java/ru/rzik/bo/kafka/KafkaConsumerService.java
+++ src/main/java/ru/rzik/bo/kafka/KafkaConsumerService.java
@@ -1,13 +1,13 @@
-package ru.rzik.bo.kafka;
-
-import org.springframework.kafka.annotation.KafkaListener;
-import org.springframework.stereotype.Service;
-
-@Service
-public class KafkaConsumerService {
+//package ru.rzik.bo.kafka;
+//
+//import org.springframework.kafka.annotation.KafkaListener;
+//import org.springframework.stereotype.Service;
+//
+//@Service
+//public class KafkaConsumerService {
 //    @KafkaListener(topics = "my-topic", groupId = "my-group")
 //    public void receiveMessage(String message) {
 //        // Process the received message
 //        System.out.println("Received message: " + message);
 //    }
-}
+//}
