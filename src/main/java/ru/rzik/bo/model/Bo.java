de74b0a26f8e01b3996e7340a1e59ea2fa716825
fmist <olejnec@gmail.com>
2025-02-05 11:18:35 +0300

    add react

--- src/main/java/ru/rzik/bo/model/Bo.java
+++ src/main/java/ru/rzik/bo/model/Bo.java
@@ -1,13 +1,11 @@
 package ru.rzik.bo.model;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.Data;
 
 @Data
 @Entity
+@Table(name = "bo")
 public class Bo {
 
     @Id
